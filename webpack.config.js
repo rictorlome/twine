f77cb76d246b0a22434154ddda2cda71771a104f
emmilco <elliot.milco@gmail.com>
2018-04-11 19:01:02 -0400

    Install webpack and its basic dependencies, set up file structure for frontend development

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./frontend/index.js",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
